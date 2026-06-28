96cec8a113354ea5cb4ef2f1f70b204ddb5d389c
Daria Polubenko <daria.polubenko@gmail.com>
2026-06-28 12:34:41 +0100

    add 2

--- src/main/java/com/example/demo/redis/aop/Service2.java
+++ src/main/java/com/example/demo/redis/aop/Service2.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Service;
 public class Service2 {
     @LogExecutionTime
     public String getUserData(int id) {
-        return "Данные пользователя с id " + id;
+        return "Данные пользователя с id  " + id;
     }
 }
